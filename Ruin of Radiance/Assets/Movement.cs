3ea5f717c512397e743181791930b0cda30fe3b5
unAlike <Ryan.Ungerleider@gmail.com>
2021-10-05 21:18:34 -0500

    Created Movement with animations

---
+++ Ruin of Radiance/Assets/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField]
+    private Rigidbody2D character;
+    private Vector2 movement;
+    [SerializeField]
+    float moveSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            Debug.Log("Shift");
+            movement = movement*2;
+        }
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.magnitude);
+    }
+
+    void FixedUpdate(){
+        character.MovePosition(character.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
