dcec09422cc54bd404d392f2a927dee3e5b422c2
unAlike <Ryan.Ungerleider@gmail.com>
2021-10-19 23:03:26 -0500

    UI

---
+++ Ruin of Radiance/Assets/GUIScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnInventoryButtonClick(){
+        Debug.Log("INV");
+    }
+    public void OnMapButtonClick(){
+        Debug.Log("Map");
+    }
+    public void OnSkillTreeButtonClick(){
+        Debug.Log("Skill Tree");
+    }
+}
