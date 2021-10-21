0ba472e3407195daf27e78381538facd05a1b97f
unAlike <Ryan.Ungerleider@gmail.com>
2021-10-21 11:00:27 -0500

    weee

--- Ruin of Radiance/Assets/Scripts/GUIScript.cs
+++ Ruin of Radiance/Assets/Scripts/GUIScript.cs
@@ -1,27 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GUIScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Button invBtn, mapBtn, sklBtn;
+    EventSystem EventSystem;
+    
+    string activeBtn;
+    [SerializeField]
+    bool openGUI = false;
     void Start()
     {
-        
+        EventSystem = GameObject.Find("InventoryUI").GetComponent<EventSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        switch(EventSystem.currentSelectedGameObject.name){
+            case "InventoryButton":
+                Debug.Log(EventSystem.currentSelectedGameObject.name);
+                openGUI=true;
+                break;
+            case "MapButton":
+                Debug.Log(EventSystem.currentSelectedGameObject.name);
+                break;
+            case "SkillButton":
+                Debug.Log(EventSystem.currentSelectedGameObject.name);
+                break;
+            case null:
+                break;
+            
+        }
+        if(openGUI){
+            OpenGui();
+        }
+        else{
+            CloseGui();
+        }
     }
-    public void OnInventoryButtonClick(){
-        Debug.Log("INV");
+    public void OpenGui(){
+        GameObject.Find("MenuPanel").transform.position = GameObject.Find("MenuPanel").transform.position + new Vector3(0,.01f,0);
     }
-    public void OnMapButtonClick(){
-        Debug.Log("Map");
+    public void CloseGui(){
+
     }
-    public void OnSkillTreeButtonClick(){
-        Debug.Log("Skill Tree");
-    }
+
 }
