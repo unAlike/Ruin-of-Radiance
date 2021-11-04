925c6030bfe006d74d967a61bea80ed772c902ab
Garrett Brown <browga20@gmail.com>
2021-11-04 11:01:19 -0500

    more code

--- Ruin of Radiance/Assets/Scripts/CombatLogic.cs
+++ Ruin of Radiance/Assets/Scripts/CombatLogic.cs
@@ -22,16 +22,58 @@ public class CombatLogic : MonoBehaviour
     List<CombatTile> units;
     Movement moveScript;
     CombatGrid grid = new CombatGrid();
+    CombatTile activeTile = new CombatTile(0,1);
+    Unit Character;
     void Start(){
         moveScript = GameObject.Find("DynamicSprite").GetComponent<Movement>();
         // moveScript.inCombat = true;
+        createPlayer();
+        
+        
+        grid.tiles[0,1].createUnit(Character);
     }    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             endCombat();
         }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            int xCoord = grid.findUnit(Character).xCoord;
+            int yCoord = grid.findUnit(Character).yCoord;
+            grid.moveUnitTo(grid.findUnit(Character),xCoord+1,yCoord);
+            Debug.Log("Unit moved right");
+    
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            int xCoord = grid.findUnit(Character).xCoord;
+            int yCoord = grid.findUnit(Character).yCoord;
+            grid.moveUnitTo(grid.findUnit(Character),xCoord,yCoord-1);
+            Debug.Log("Unit moved down");
+    
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            int xCoord = grid.findUnit(Character).xCoord;
+            int yCoord = grid.findUnit(Character).yCoord;
+            grid.moveUnitTo(grid.findUnit(Character),xCoord,yCoord+1);
+            Debug.Log("Unit moved up");
+    
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            int xCoord = grid.findUnit(Character).xCoord;
+            int yCoord = grid.findUnit(Character).yCoord;
+            grid.moveUnitTo(grid.findUnit(Character),xCoord-1,yCoord);
+            Debug.Log("Unit moved left");
+    
+        }
+        
         // Debug.Log("character Position: " + GameObject.Find("DynamicSprite").transform.position);
+
+
+    }
+    public void createPlayer(){
+        Character = new Unit();
+        Character.unitSprite = GameObject.Find("DynamicSprite"); 
+
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
@@ -45,18 +87,16 @@ public class CombatLogic : MonoBehaviour
         gridPos = GameObject.Find("CombatGrid").GetComponent<SpriteRenderer>().transform.position + charStart;
         charSprite.transform.position = gridPos;
     
-        Unit Character = new Unit();
-        grid.tiles[0,1].createUnit(Character); // places character on [0,1]
+        // Unit Character = new Unit();
+        // grid.tiles[0,1].createUnit(Character); // places character on [0,1]
         grid.tiles[6,2].createUnit(units[2].tileUnit); // places enemies 1,2,3 on their locations
         grid.tiles[6,1].createUnit(units[1].tileUnit);
         grid.tiles[6,0].createUnit(units[0].tileUnit);
-
-
+        grid.tiles[5,2].createUnit(units[3].tileUnit); // places enemies 4,5,6 on their locations
+        grid.tiles[5,1].createUnit(units[4].tileUnit);
+        grid.tiles[5,0].createUnit(units[5].tileUnit);
 
         // place the player on [0,1]
-        
-
-        
         // units[1].tileUnit.getHealth();
         
         // toggle movement off
@@ -92,22 +132,42 @@ public class CombatGrid{
 
     public CombatGrid(){
         tiles = new CombatTile[7,3];
-
+        for (int i = 0; i < 7;++i) {
+            for (int j = 0; j < 3;++j) {
+            tiles[i,j] = new CombatTile(i,j);
+            }
+        }
+       
     }
+
     public void moveUnitTo(CombatTile unit1, int xCoord, int yCoord) {
         // play animation to move sprite
         // moving distance, limits player to that distance
         // highlightTiles(unit1.xCoord, unit1.yCoord);
-        if(!tiles[xCoord,yCoord].getIsOccupied()){ // if not occupied - originally .tileUnit.getIsOccupied() n
+        if(!tiles[xCoord,yCoord].getIsOccupied() && xCoord < 7 && xCoord > -1 && yCoord < 3 && yCoord > -1 ){ // if not occupied - originally .tileUnit.getIsOccupied() n
             tiles[xCoord,yCoord].tileUnit = unit1.tileUnit; // copy unit over then DELETE OLD SPOT
             unit1.deleteUnit(); // deletes the prev unit
-        
-
+            Debug.Log("Moved Unit to " + unit1.xCoord + ", " + unit1.yCoord);
         }
         else { 
-             Debug.Log(" space already taken.");
+             Debug.Log(" space already taken or out of bounds");
         }
          
+    }
+    public CombatTile findUnit(Unit unit) {
+        for (int i = 0; i < 7;++i) {
+
+            for (int j = 0; j < 3;++j) {
+            // tiles[i,j];
+                if (tiles[i,j].tileUnit == unit) {
+                    Debug.Log("Unit Found");
+                    return tiles[i,j];
+                }
+            }
+        }
+        Debug.Log("Unit not Found");
+        return tiles[-1,-1];
+
     }
     public void highlightTiles(int xCoord, int yCoord) {
         // highlights gridUnit (xCoord + ActionPoints, yCoord)
@@ -123,8 +183,11 @@ public class CombatTile{
     public int xCoord, yCoord;
     private bool isOccupied;
 
-    public CombatTile(){
+    public CombatTile(int x, int y){
         tileUnit = new Unit();
+        xCoord = x;
+        yCoord = y;
+
     }
     public void setIsOccupied(bool set){
         isOccupied = set;
