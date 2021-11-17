fdc7364a0840125d57ad30c1065c785683bbe285
Garrett Brown <browga20@gmail.com>
2021-11-16 23:05:24 -0600

    addedd comment

--- Ruin of Radiance/Assets/Scripts/Combat/CombatTile.cs
+++ Ruin of Radiance/Assets/Scripts/Combat/CombatTile.cs
@@ -46,6 +46,8 @@ public class CombatTile{
     public void setHighlight(int highlight1) {
         highlight = highlight1;
         // 0 - none, 1 - move, 2 - damage, 3 - MC, 4 - SelectedTile
+        // Image
+    
     }
     public int getHighlight() {
 
